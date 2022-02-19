98835a3db25feee10290ee7014f227f2572c49be
alinakuznetsovaa <43295104+alinakuznetsovaa@users.noreply.github.com>
2022-02-19 15:56:48 +0300

    Delete src/main/java/com/netcracker/repository directory

--- src/main/java/com/netcracker/repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.netcracker.repository;
-
-import com.netcracker.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<Service, Integer> {
-}
